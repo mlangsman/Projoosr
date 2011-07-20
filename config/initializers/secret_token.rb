d4a62ee3e91ef9b841c44a5fe08dac62c68881c9
Marc Langsman <marc@langsman.com>
2011-07-20 20:06:20 +0100

    Basic user authentication implemented with Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projoosr::Application.config.secret_token = '03941e2b94a3a6b9607dc9438acfb35383c8b14ef1d4a09db51b07fa2f3efe508529182f70d906be3f5651423889205cd73e217bb52dddaf7668cb9c0b9b0377'
