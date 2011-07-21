759bf8a4e6c27642081249d9f12a97552fd4ecda
Marc Langsman <marc@langsman.com>
2011-07-21 09:55:35 +0100

    Fixed error line in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Projoosr::Application.initialize!
 
-config.gem "panda", :version => ">= 1.0.0", :source => "http://gemcutter.org"
-
