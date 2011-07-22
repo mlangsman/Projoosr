54eedd9ef999dd2f339ad12864b72010480eb441
Marc Langsman <marc@langsman.com>
2011-07-22 18:07:34 +0100

    Site index view only shows published videos

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,7 @@
 class SiteController < ApplicationController
   def index
-    @videos = Video.find(:all)
+    #@videos = Video.find(:all)
+    @videos = Video.find_all_by_published(true)
   end
 
   def about
