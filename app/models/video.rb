f7c52c0f71b220b6568a8739ed2c16d11c93b485
Marc Langsman <marc@langsman.com>
2011-07-22 15:19:55 +0100

    Added thumbnail action to video model which returns the 1st panda thumbnail or otherwise a placeholder.

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,5 +5,9 @@ class Video < ActiveRecord::Base
   def panda_video
     @panda_video ||= Panda::Video.find(panda_video_id)
   end
+  
+  def thumbnail
+    panda_video.encodings.find_by_profile_name("h264").screenshots.first || "/images/placeholder_thumbnail.png"
+  end
 
 end
