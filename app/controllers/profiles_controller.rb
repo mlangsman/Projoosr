f0e754e329c1f5210be543357b44e8ca2b45de67
Marc Langsman <marc@langsman.com>
2011-07-30 12:07:45 +0100

    Implemented avatars and image upload on profiles. Fixed routing on editing profiles. Added flash messages for unpublished videos

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,14 @@
 class ProfilesController < ApplicationController
-  #before_filter :authenticate_user!, :except => [:show]
+  before_filter :authenticate_user!, :except => [:show, :new, :create]
 
   def show
     @profile = current_user.profile
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @profile }
     end
   end
 
-  # GET /profiles/new
-  # GET /profiles/new.xml
   def new
     @profile = Profile.new
 
@@ -21,47 +18,38 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # GET /profiles/1/edit
   def edit
     @profile = current_user.profile
   end
 
-  # POST /profiles
-  # POST /profiles.xml
   def create
     @profile = Profile.new(params[:profile])
 
     respond_to do |format|
       if @profile.save
-        format.html { redirect_to(@profile, :notice => 'Profile was successfully created.') }
-        format.xml  { render :xml => @profile, :status => :created, :location => @profile }
+        format.html { redirect_to :action => "show", :notice => 'Profile was successfully created.' }
+       # format.xml  { render :xml => @profile, :status => :created, :location => @profile }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @profile.errors, :status => :unprocessable_entity }
+      #  format.xml  { render :xml => @profile.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /profiles/1
-  # PUT /profiles/1.xml
   def update
-    @profile = Profile.find(params[:id])
+    @profile = current_user.profile
 
-    respond_to do |format|
       if @profile.update_attributes(params[:profile])
-        format.html { redirect_to(@profile, :notice => 'Profile was successfully updated.') }
-        format.xml  { head :ok }
+        flash[:notice] = "Profile was successfully updated."
+        redirect_to :action => "show"
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @profile.errors, :status => :unprocessable_entity }
+        flash[:error] = "Something went wrong ! Profile was not updated."
+        redirect_to :action => "edit"
       end
-    end
   end
 
-  # DELETE /profiles/1
-  # DELETE /profiles/1.xml
   def destroy
-    @profile = Profile.find(params[:id])
+    @profile = current_user.profile
     @profile.destroy
 
     respond_to do |format|
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -9,15 +9,23 @@ class VideosController < ApplicationController
     def show
       begin
         @video = Video.find(params[:id])
-        # If the requested video isnt published yet we return a 404 
+        # If the requested video isnt published 
         if @video.published?
           @original_video = @video.panda_video
           @h264_encoding = @original_video.encodings.find_by_profile_name("h264") 
         else 
-          render_404
+          #render_404
+          respond_to do |format|
+            if @video.user = current_user
+              flash.now[:notice] = "Your video is being encoded and reviewed. We'll let you know as soon as it's ready!"
+            else
+              flash.now[:notice] = "This video has not been published yet. Check back soon..."
+            end
+            format.html { render :template => "videos/unavailable", :layout => true }
+          end
         end
-      rescue ActiveRecord::RecordNotFound
-        render_404
+        rescue ActiveRecord::RecordNotFound
+          render_404
       end
     
       
