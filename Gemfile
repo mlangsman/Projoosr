e2e5e2d9cfdf125758db56f207fdbf692dc8713b
Marc Langsman <marc@langsman.com>
2011-08-01 13:26:54 +0100

    Implemented random unique filenames for avatars and thumbnail versions. Added avatar to video view.

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sqlite3'
 gem 'devise'
 gem 'panda'
 gem 'carrierwave'
+gem 'rmagick'
 
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,7 +3,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or ImageScience support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
@@ -22,22 +22,48 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  #process :scale => [300, 300]
+  
+  process :resize_to_fill => [300, 300]
+  process :convert => 'png'
+  
+  version :thumb do 
+    process :resize_to_fill => [75, 75] 
+    process :convert => 'png' 
+  end
+  
+  version :tiny do 
+    process :resize_to_fill => [30, 30] 
+    process :convert => 'png' 
+  end
+  
+  #Create a unique random filename
+  def filename
+     "#{secure_token}.png" if original_filename.present?
+  end
+
+  protected
+    def secure_token
+      var = :"@#{mounted_as}_secure_token"
+      model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(10))
+    end
+  
+  
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  #version :thumb do
+   #  process :scale => [50, 50]
+  #end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_white_list
+     %w(jpg jpeg gif png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
