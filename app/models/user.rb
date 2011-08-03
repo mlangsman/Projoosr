99265527838d987c4744302039bd72c5b67e7178
Marc Langsman <marc@langsman.com>
2011-08-03 16:21:29 +0100

    Added validation ensure presence and uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  
+  validates_presence_of :username
+  validates_uniqueness_of :username
   
   protected
   
